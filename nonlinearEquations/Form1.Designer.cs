373fc7bd81648a6d7bcb0d1e2267822ed77310b7
andre <andre@ANDREY-PC>
2024-12-18 22:46:39 +0300

    Добавьте файлы проекта.

---
+++ nonlinearEquations/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace nonlinearEquations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dichotomy method";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(498, 381);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(522, 228);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(125, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "y = (x-1) * (x+7) * (x-2)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.radioButton1_MouseDown);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(522, 273);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(95, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "y = (x-3) * (x+4)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.radioButton2_MouseDown);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(522, 310);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "y = 2x +1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.radioButton3_MouseDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 277);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 330);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Nuton";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Input a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Input b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Input infelicity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ nonlinearEquations/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace nonlinearEquations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = -1;
+            try
+            {
+                a = Convert.ToDouble(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input a", "Worning");
+                return;
+            }
+            double b = 1;
+            try
+            {
+                b = Convert.ToDouble(textBox4.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input b", "Worning");
+                return;
+            }
+            double eps = 0;
+            try
+            {
+                eps = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input infelicity", "Worning");
+                return;
+            }
+            f func;
+            double res;
+            if (radioButton1.Checked)
+            {
+                func = f1;
+                res = DichotomyMethod(a, b, func, eps);
+            }
+            else if (radioButton2.Checked)
+            {
+                func = f2;
+                res = DichotomyMethod(a, b, func, eps);
+            }
+            else if (radioButton3.Checked)
+            {
+                func = f3;
+                res = DichotomyMethod(a, b, func, eps);
+            }
+            else
+            {
+                MessageBox.Show("Check function", "Worning");
+                return;
+            }
+            textBox2.Text = Convert.ToString(res);
+        }
+        public double DichotomyMethod(double a, double b, f func, double eps)
+        {
+            double x = (b+a)/2;
+            if(func(x) == 0)
+            {
+                return x;
+            }
+            else if(func(a) == 0)
+            {
+                return a;
+            }
+            else if (func(b) == 0)
+            {
+                return b;
+            }
+            if (func(a)*func(x) < 0)
+            {
+                b=x;
+            }
+            else if(func(x)*func(b) < 0)
+            {
+                a=x;
+            }
+            else
+            {
+                a /= 2;
+                b /= 2;
+            }
+            if(Math.Abs(func(x)) < eps)
+            {
+                return x;
+            }
+            else
+            {
+                return DichotomyMethod(a, b, func, eps);
+            }
+        }
+        public double Nuton(double x, double h, f func)
+        {
+            return x - func(x)/dfdx(x, h, func);
+        }
+        public double dfdx(double x, double h, f func)
+        {
+            return (func(x+h) - func(x))/h;
+        }
+        public delegate double f(double x);
+        public double f1(double x)
+        {
+            return x*x*x + 4*x*x - 19*x + 14;
+        }
+        public double f2(double x)
+        {
+            return x*x + x - 12;
+        }
+        public double f3(double x)
+        {
+            return 2*x+1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = "-10";
+            textBox4.Text = "10";
+            textBox1.Text = "0,001";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = -1;
+            try
+            {
+                a = Convert.ToDouble(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input a", "Worning");
+                return;
+            }
+            double b = 1;
+            try
+            {
+                b = Convert.ToDouble(textBox4.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input b", "Worning");
+                return;
+            }
+            double eps = 0;
+            try
+            {
+                eps = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input infelicity", "Worning");
+                return;
+            }
+            f func;
+            double h = 0.0001;
+            double x0;
+            double xNext = (a+b)/2;
+            if (radioButton1.Checked)
+            {
+                func = f1;
+            }
+            else if (radioButton2.Checked)
+            {
+                func = f2;
+            }
+            else if (radioButton3.Checked)
+            {
+                func = f3;
+            }
+            else
+            {
+                MessageBox.Show("Check function", "Worning");
+                return;
+            }
+            do
+            {
+                x0 = xNext;
+                xNext = Nuton(x0, h, func);
+            } while (Math.Abs(xNext - x0) >= eps);
+            textBox2.Text = Convert.ToString(x0);
+        }
+        private void radioButton1_MouseDown(object sender, MouseEventArgs e)
+        {
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton2_MouseDown(object sender, MouseEventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton3_MouseDown(object sender, MouseEventArgs e)
+        {
+            radioButton2.Checked = false;
+            radioButton1.Checked = false;
+        }
+    }
+}
